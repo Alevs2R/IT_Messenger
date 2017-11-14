230536e19d40806f322441226ff4dc79bddb8bff
arrilight <g.dzesov@gmail.com>
2017-11-14 18:58:27 +0300

    Noise generator added

---
+++ src/NoiseGenerator.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class NoiseGenerator {
+    /**
+     * This function takes byte array as an input and performs some flipping of bits
+     * with probability of epsilon.
+     * @param array is the input byte array
+     * @param epsilon is the probability of flipping a bit
+     */
+    void generateNoise(byte[] array, double epsilon) {
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            StringBuilder newValue = new StringBuilder();
+            for (int j = 0; j < 8; j++) {
+                byte temp = array[i];
+                int value = (temp >>> (j)) & 1;
+                if (epsilon >= random.nextDouble())
+                    value = value == 1 ? 0 : 1;
+                else
+                    value = value == 1 ? 1 : 0;
+                newValue.append(value);
+            }
+            array[i] = (byte)Integer.parseInt(newValue.reverse().toString());
+        }
+    }
+}
