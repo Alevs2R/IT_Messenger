d4b28032443008caf78cfcc3c7a4a6c5e48627e6
Victoria <vica.rotaru97@gmail.com>
2017-11-18 18:40:37 +0300

    added comments

--- src/RunLengthCompression.java
+++ src/RunLengthCompression.java
@@ -1,8 +1,28 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
+/**
+ * Run Length Encoding is a lossless algorithm that it is only efficient
+ * with files that contain lots of repetitive data.
+ */
 class RunLengthCompression {
 
-    static Byte[] compress(byte[] binary) {
+    /**
+     * This function takes a file, transforms it into bytes then compresses it with RLE algorithm.
+     * The algorithm replaces a sequence of repeating symbols with a single symbol and a count (run length)
+     * indicating the number of times the symbol is repeated.
+     *
+     * @param fileName the name of the file that should be compressed
+     * @return a compressed byte array
+     */
+    static byte[] compress(String fileName) throws IOException {
+        File initialFile = new File(fileName);
+        InputStream targetStream = new FileInputStream(initialFile);
+        byte[] binary = org.apache.commons.io.IOUtils.toByteArray(targetStream);
+        System.out.println("Size before compression: " + ((double) binary.length / 1024) / 1024 + " MBytes");
         ArrayList<Byte> compressed = new ArrayList<>();
         int counter = 1;
         for (int i = 0; i < binary.length; i++) {
@@ -18,10 +38,28 @@ class RunLengthCompression {
             compressed.add(binary[i]);
             counter = 1;
         }
-        return (Byte[]) compressed.toArray();
+        Byte[] comp = new Byte[compressed.size()];
+        comp = compressed.toArray(comp);
+        byte[] compr;
+        compr = toPrimitives(comp);
+        System.out.println("Size after compression: " + ((double) compr.length / 1024) / 1024 + " MBytes");
+        return compr;
     }
 
-    static Byte[] decompress(byte[] compressed) {
+    //Byte[] to byte[]
+    private static byte[] toPrimitives(Byte[] oBytes) {
+        byte[] bytes = new byte[oBytes.length];
+        for (int i = 0; i < oBytes.length; i++) {
+            bytes[i] = oBytes[i];
+        }
+        return bytes;
+    }
+
+    /**
+     * @param compressed the file that should be decompressed
+     * @return an array of decompressed bytes
+     */
+    static byte[] decompress(byte[] compressed) {
         ArrayList<Byte> decompressed = new ArrayList<>();
         byte tmp;
         int count = 0;
@@ -38,7 +76,11 @@ class RunLengthCompression {
             i++;
             count = 0;
         }
-        return (Byte[]) decompressed.toArray();
-
+        Byte[] decomp = new Byte[decompressed.size()];
+        decomp = decompressed.toArray(decomp);
+        byte[] decompr;
+        decompr = toPrimitives(decomp);
+        return decompr;
     }
+
 }
